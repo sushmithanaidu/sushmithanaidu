988cd76f3fb13147fdc58af7e541884f09cb1c31
sushmithanaidu <40797034+sushmithanaidu@users.noreply.github.com>
2018-07-12 14:51:15 +0530

    update 4.py

--- 4.py
+++ 4.py
@@ -1,9 +1,6 @@
-num1 = 1
-num2 = 2
-num3 = 3
-#num1 = float(input("Enter first number: "))
-#num2 = float(input("Enter second number: "))
-#num3 = float(input("Enter third number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
 
 if (num1 >= num2) and (num1 >= num3):
    largest = num1
