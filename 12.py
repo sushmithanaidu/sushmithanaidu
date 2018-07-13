6120145522b6b28d086f601cd1a7773082bc3c35
sushmithanaidu <40797034+sushmithanaidu@users.noreply.github.com>
2018-07-13 12:39:07 +0530

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,12 @@
+num=int(raw_input("Enter a number"))
+sum1=0
+n=num
+ 
+while num!=0:
+	rem=num%10
+	sum1=sum1*10+rem
+	num=num/10
+if sum1==n:
+	print n, "is palindrome"
+else:
+	print n, "is not palindrome"
